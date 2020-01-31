18e05daf0ed93cc7c4e9c7af364a2d8108f936ca
Steven Sewell <stevenrsewell@outlook.com>
2020-01-30 23:56:14 -0600

    employeetracker.js require and connection

---
+++ employeetracker.js
@@ -0,0 +1,15 @@
+var mysql = require('mysql')
+var inquirer = require('inquirer')
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'employeetracker_db'
+})
+
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+});
