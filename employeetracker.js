26ae2b740a2e42bcbce298fcc267e5463ab2ec69
Steven Sewell <stevenrsewell@outlook.com>
2020-03-06 18:44:00 -0600

    Removed node_modules, updated ReadMe

--- employeetracker.js
+++ employeetracker.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '',
+  password: '1234CanadaDeservesMore$',
   database: 'employeetracker_db'
 })
 
@@ -13,3 +13,62 @@ connection.connect(function(err) {
     if (err) throw err;
     start();
 });
+
+
+//
+// connection.query('select * from employee', function (error, results, fields) {
+//
+//   console.log(results)
+// })
+const promtActions = {
+    "Add employee": () => {
+      connection.query('select * from roles',(err, res, fields) => {
+        let roleOptions = res.map(option => {
+          return {[option.title]: option.id}
+        })
+        let inquirerRoleOptions = {
+          name: "roleChoiceId",
+          message: "What employee role?",
+          type: "list",
+          choices: []
+        }
+        roleOptions.forEach((roleOption, i) => {
+          console.log((roleOption));
+          inquirerRoleOptions.choices.push({name: Object.keys(roleOption)[0], value: roleOption[Object.keys(roleOption)[0]]})
+        });
+        console.log(inquirerRoleOptions);
+        inquirer.prompt(
+          [
+            {
+              name: "firstName",
+              message: "Employee first name?",
+              type: "input"
+            },
+            {
+              name: "lastName",
+              message: "Employee last name?",
+              type: "input"
+            },
+            inquirerRoleOptions
+          ]
+        ).then(answers=>{
+          console.log(answers);
+        })
+      })
+    },
+    "Add department": () => {
+    },
+    "Quit": () => {
+      connection.end()
+      console.log("Goodbye!")
+    },
+  }
+  inquirer.prompt({
+    name: "firstPrompt",
+    message: "What do you want to do?",
+    type: "list",
+    choices: ["Add employee", "Add department", "Quit"]
+  }).then(answers => {
+    promtActions[answers[Object.keys(answers)]]()
+  })
+  
